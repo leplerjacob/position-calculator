6b40415f8cd410b5c31c79a70bfd673a555d2c7d
Jacob Lepler <lepler.jacob@gmail.com>
2021-09-01 11:47:18 -0400

    import new history table component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import CalcForm from './components/CalcForm'
-import './App.css'
+import CalcForm from "./components/CalcForm";
+import History from "./components/History";
+import { useState } from "react";
+import "./App.css";
 
-// This is a test comment
+/* Position formula
+  Trade amount = Risk / (Stoploss / 100)
+*/
+
+const tempData = [
+  ["BTC", 1000, 2, 47500, 47250, 0.53, 3773],
+  ["ETH", 1000, 2, 3500, 3350, 4.29, 466],
+];
+
 function App() {
+  const [history, setHistory] = useState(tempData);
+
   return (
     <div className="App">
       <CalcForm />
+      <History history={history} />
     </div>
   );
 }
