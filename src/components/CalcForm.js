9dd50be0daf747d732f71daff0416f38bf8c2f7e
Jacob Lepler <lepler.jacob@gmail.com>
2021-09-01 11:46:50 -0400

    add chosen crypto

--- src/components/CalcForm.js
+++ src/components/CalcForm.js
@@ -5,12 +5,22 @@ const calcForm = () => {
   return (
     <div className="form">
       <Form>
+        <Form.Select className="form-input mb-3" aria-label="Default select example">
+          <option>Select Crypto</option>
+          <option value="btc">BTC</option>
+          <option value="eth">ETH</option>
+        </Form.Select>
+
         <FloatingLabel
           controlId="floatingInput"
           label="Capital"
           className="mb-3"
         >
-          <Form.Control type="capital" placeholder="capital" className />
+          <Form.Control
+            className="form-input"
+            type="capital"
+            placeholder="capital"
+          />
         </FloatingLabel>
 
         <FloatingLabel
@@ -18,26 +28,30 @@ const calcForm = () => {
           label="Percentage of risk"
           className="mb-3"
         >
-          <Form.Control type="risk" placeholder="risk" />
+          <Form.Control className="form-input" type="risk" placeholder="risk" />
         </FloatingLabel>
 
-        <FloatingLabel
-          controlId="entry"
-          label="Entry price"
-          className="mb-3"
-        >
-          <Form.Control type="price" placeholder="Entry price" />
+        <FloatingLabel controlId="entry" label="Entry price" className="mb-3">
+          <Form.Control
+            className="form-input"
+            type="price"
+            placeholder="Entry price"
+          />
         </FloatingLabel>
 
-        <FloatingLabel
-          controlId="stopLoss"
-          label="Stop/Loss"
-          className="mb-3"
-        >
-          <Form.Control type="stopLoss" placeholder="Stop/Loss" />
+        <FloatingLabel controlId="stopLoss" label="Stop loss price" className="mb-3">
+          <Form.Control
+            className="form-input"
+            type="stopLoss"
+            placeholder="Stop loss price"
+          />
         </FloatingLabel>
 
-        <Form.Control type="calculatedPosition" placeholder="00.00"/>
+        <Form.Control
+          className="form-input"
+          type="calculatedPosition"
+          placeholder="00.00"
+        />
       </Form>
     </div>
   );
