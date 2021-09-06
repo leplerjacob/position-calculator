85bf71a8ea15c4524101070c2b80cba554232902
Jacob Lepler <lepler.jacob@gmail.com>
2021-09-06 16:27:12 -0400

    feat(form: added inputs and position eq

--- src/components/CalcForm.js
+++ src/components/CalcForm.js
@@ -1,35 +1,83 @@
 import Form from "react-bootstrap/Form";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import { Component, useState } from "react";
 
 class CalcForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ticker: "BTC",
+      crypto: "BTC",
+      position: "position",
       capital: 1000,
       risk: 1.0,
       entry: 0.0,
       stop: 0.0,
+      calculatedPosition: 0.0,
     };
   }
 
   handleChange = (e) => {
-    this.setState({[e.target.name]: e.target.value})
-  }
+    console.log(e.target.value, e.target.name);
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  calculatePosition = async (e) => {
+    e.preventDefault();
+    const { crypto, position, capital, risk, stop, entry, calculatedPosition } =
+      this.state;
+    if (position == "short") {
+      const result = (capital * risk) / (stop / entry - 1) / 100;
+      await this.setState({ calculatedPosition: Math.round(result) });
+    }
+    if (position == "long") {
+      const result = (capital * risk) / (entry / stop - 1) / 100;
+      await this.setState({ calculatedPosition: Math.round(result) });
+    }
+
+    this.props.handleCalc({
+      crypto,
+      position,
+      capital,
+      risk,
+      stop,
+      entry,
+      calculatedPosition,
+    });
+  };
 
   render() {
     return (
       <div className="form">
-        <Form>
-          <Form.Select
-            className="form-input mb-3"
-            aria-label="Default select example"
-          >
-            <option>Select Crypto</option>
-            <option value="btc">BTC</option>
-            <option value="eth">ETH</option>
-          </Form.Select>
+        <Form onSubmit={this.calculatePosition}>
+          <Row>
+            <Col>
+              <Form.Select
+                className="form-input mb-3"
+                aria-label="select crypto"
+                onChange={this.handleChange}
+                name="crypto"
+              >
+                <option>Select Crypto</option>
+                <option value="btc">BTC</option>
+                <option value="eth">ETH</option>
+              </Form.Select>
+            </Col>
+            <Col>
+              <Form.Select
+                className="form-input mb-3"
+                aria-label="select position"
+                onChange={this.handleChange}
+                name="position"
+              >
+                <option>Select Side</option>
+                <option value="short">Short</option>
+                <option value="long">Long</option>
+              </Form.Select>
+            </Col>
+          </Row>
 
           <FloatingLabel
             controlId="floatingInput"
@@ -56,6 +104,8 @@ class CalcForm extends Component {
               type="risk"
               placeholder="risk"
               name="risk"
+              value={this.state.risk}
+              onChange={this.handleChange}
             />
           </FloatingLabel>
 
@@ -65,6 +115,8 @@ class CalcForm extends Component {
               type="price"
               placeholder="Entry price"
               name="entry"
+              value={this.state.entry}
+              onChange={this.handleChange}
             />
           </FloatingLabel>
 
@@ -78,15 +130,18 @@ class CalcForm extends Component {
               type="stopLoss"
               placeholder="Stop loss price"
               name="stop"
+              value={this.state.stop}
+              onChange={this.handleChange}
             />
           </FloatingLabel>
 
           <Form.Control
             className="form-input"
             type="calculatedPosition"
-            placeholder="00.00"
+            value={this.state.calculatedPosition}
             disabled
           />
+          <Button type="submit">Submit</Button>
         </Form>
       </div>
     );
