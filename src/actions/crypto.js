77be9e8ee55d7fd9537827b4489e7a03602b544e
Jacob Lepler <lepler.jacob@gmail.com>
2021-09-10 16:55:57 -0400

    fix promise error

--- src/actions/crypto.js
+++ src/actions/crypto.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
 
 export async function getBTCPrice() {
-    let data = await axios.get('http://localhost:3000/btcusd')
+    let data = await axios.get('http://localhost:3001/btcusd')
     return data
 }
+
+export async function getETHPrice() {
+    let data = await axios.get('http://localhost:3001/ethusd')
+    return data
+}
+
