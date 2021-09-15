8282eb58b6ae2faed355d0ab2ac20ba8f5d613e3
Jacob Lepler <lepler.jacob@gmail.com>
2021-09-15 10:21:21 -0400

    no change

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import CalcForm from "./components/CalcForm";
 import History from "./components/History";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const tempData = [
-  {crypto: "BTC", capital: 1000, risk: 2, entry:47500, stop: 47250, stopPercentage: 0.53, calculatedPosition: 3773, position: "long"},
-  {crypto: "BTC", capital: 1000, risk: 2, entry:47500, stop: 47250, stopPercentage: 0.53, calculatedPosition: 3773, position: "long"},
+  {
+    index: 1,
+    crypto: "BTC",
+    capital: 1000,
+    risk: 2,
+    entry: 47500,
+    stop: 47250,
+    stopPercentage: 0.53,
+    calculatedPosition: 3773,
+    position: "long",
+  },
 ];
 
-function App() {
+const App = () => {
   const [history, setHistory] = useState(tempData);
 
-  const handleCalc = (trade) => {
-    console.log(trade)
-    setHistory([...history, trade])
-  }
-  
+  const handleCalc = async (trade) => {
+    trade["index"] = history.length + 1;
+    await setHistory([...history, trade]);
+  };
+
   return (
     <div className="App">
       <CalcForm handleCalc={handleCalc} />
       <History history={history} />
     </div>
   );
-}
+};
 
 export default App;
