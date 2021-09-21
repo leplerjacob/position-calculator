935ace26ce5e05516295202a67d189401570d005
Jacob Lepler <lepler.jacob@gmail.com>
2021-09-21 19:02:12 -0400

    add TODO

--- src/actions/crypto.js
+++ src/actions/crypto.js
@@ -1,12 +1,11 @@
-import axios from 'axios'
+import axios from "axios";
 
 export async function getBTCPrice() {
-    let data = await axios.get('http://localhost:3001/btcusd')
-    return data
+  let data = await axios.get("http://localhost:3001/btcusd");
+  return data;
 }
 
 export async function getETHPrice() {
-    let data = await axios.get('http://localhost:3001/ethusd')
-    return data
+  let data = await axios.get("http://localhost:3001/ethusd");
+  return data;
 }
-
